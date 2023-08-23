6d2b805a458be1f79dbaf66dd3666ba299f18695
Jackson Bean <jbean@professionalbuilding.net>
2023-08-23 16:03:07 -0500

    fix: path weights

--- path/index.mjs
+++ path/index.mjs
@@ -1,6 +1,6 @@
 import path from "node:path";
 
-export const WEIGHTS = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+export const WEIGHTS = [100, 200, 300, 400, 500, 600, 700];
 export const STYLES = ['outlined', 'rounded', 'sharp'];
 
 export const rootDir = path.resolve('..');
